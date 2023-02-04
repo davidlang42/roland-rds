63e7477125b30e2831db59487e70279d5aa9cbff
David Lang <davidlang42@gmail.com>
2023-02-04 21:57:04 +1100

    Fix error

--- src/json/serialize_chars_as_string.rs
+++ src/json/serialize_chars_as_string.rs
@@ -5,7 +5,11 @@ where D: serde::Deserializer<'de>
 {
     let s = <&str>::deserialize(deserializer)?;
     let chars: Vec<char> = s.chars().collect();
-    Ok(chars.try_into().unwrap()) //TODO handle error .map_err(de::Error::custom)
+    if chars.len() != N {
+        Err(de::Error::custom(format!("Expected {} chars, but got {}", N, chars.len())))
+    } else {
+        Ok(chars.try_into().unwrap())
+    }
 }
 
 pub fn serialize<S, const N: usize>(chars: &[char; N], serializer: S) -> Result<S::Ok, S::Error>
