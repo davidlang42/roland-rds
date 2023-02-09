b6bd10cfe18bcf7cfb385ce4b3715ddf5738054d
David Lang <davidlang42@gmail.com>
2023-02-10 03:02:40 +1100

    Implement VLink

--- src/roland/system/mod.rs
+++ src/roland/system/mod.rs
@@ -3,20 +3,23 @@ use crate::bytes::{Bytes, BytesError, StructuredJson};
 use crate::json::serialize_chars_as_string;
 use self::common::Common;
 use self::favorites::Favorites;
+use self::v_link::VLink;
 
 use super::validate;
 
 mod favorites;
 mod common;
+mod v_link;
 
 #[derive(Serialize, Deserialize)]
 pub struct System {
-    unused: Bits<16>, // 2 bytes
+    unused1: Bits<16>, // 2 bytes
     common: Common, // 8 bytes
-    compressor: Bits<256>, // 103 bits, 32 bytes?
+    compressor: Bits<192>, // 103 bits, 24 bytes?
+    v_link: VLink, // 4 bytes
+    unused2: Bits<32>, // 4 bytes
     favorites: Favorites, // 76 bytes
-    v_link: Bits<48>, // 30 bits, 4 bytes?
-    switch_assign: Bits<160>, // 150 bits, 20 bytes?
+    switch_assign: Bits<208>, // 150 bits, 20 bytes?
     #[serde(with = "serialize_chars_as_string")]
     hardware_version: [char; 16]
 }
@@ -24,39 +27,44 @@ pub struct System {
 impl Bytes<160> for System {
     fn from_bytes(bytes: Box<[u8; Self::BYTE_SIZE]>) -> Result<Self, BytesError> {
         let mut data = BitStream::read(bytes);
-        let unused = data.get_bits();
+        let unused1 = data.get_bits();
         let common = Common::from_bytes(Box::new(data.get_bytes()))?;
         let compressor = data.get_bits();
+        let v_link = VLink::from_bytes(Box::new(data.get_bytes()))?;
+        let unused2 = data.get_bits();
         let favorites = Favorites::from_bytes(Box::new(data.get_bytes()))?;
-        let v_link = data.get_bits();
         let switch_assign = data.get_bits();
         let mut hardware_version = [char::default(); 16];
         for i in 0..hardware_version.len() {
             hardware_version[i] = validate(data.get_u8::<8>() as char)?;
         }
         Ok(Self {
-            unused,
+            unused1,
             common,
             compressor,
-            favorites,
             v_link,
+            unused2,
+            favorites,
             switch_assign,
             hardware_version
         })
     }
 
     fn to_bytes(&self) -> Box<[u8; Self::BYTE_SIZE]> {
-        let mut bytes = self.unused.to_bytes();
+        let mut bytes = self.unused1.to_bytes();
         for byte in *self.common.to_bytes() {
             bytes.push(byte);
         }
         for byte in self.compressor.to_bytes() {
             bytes.push(byte);
         }
-        for byte in *self.favorites.to_bytes() {
+        for byte in *self.v_link.to_bytes() {
             bytes.push(byte);
         }
-        for byte in self.v_link.to_bytes() {
+        for byte in self.unused2.to_bytes() {
+            bytes.push(byte);
+        }
+        for byte in *self.favorites.to_bytes() {
             bytes.push(byte);
         }
         for byte in self.switch_assign.to_bytes() {
