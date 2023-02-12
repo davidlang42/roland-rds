f925dcd47f8b49f57e6e3bcbd4e5e40461fa133d
David Lang <davidlang42@gmail.com>
2023-02-13 07:47:59 +1100

    Basic implement of MFX

--- src/roland/live_set/mfx.rs
+++ src/roland/live_set/mfx.rs
@@ -2,21 +2,43 @@ use std::fmt::Debug;
 
 use crate::bits::{Bits, BitStream};
 use crate::bytes::{Bytes, BytesError, StructuredJson};
+use crate::roland::in_range_u16;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Mfx(Bits<608>);
-//TODO (definitely relevant) fields are partially defined by the 700NX midi implementation, but there is a lot of extra space unaccounted for (should be 521 bits) maybe some of it is checksum?
+pub struct Mfx {
+    enable: bool,
+    effect_type: Bits<92>, //TODO decode effect type (needs new test RDS file)
+    parameters: [u16; 32], // 12768-52768 (-20000 - +20000)
+    #[serde(skip_serializing_if="Bits::is_zero", default="Bits::zero")]
+    unused: Bits<3>
+}
 
 impl Bytes<76> for Mfx {
     fn to_bytes(&self) -> Box<[u8; Self::BYTE_SIZE]> {
         BitStream::write_fixed(|bs| {
-            bs.set_bits(&self.0);
+            bs.set_bool(self.enable);
+            bs.set_bits(&self.effect_type);
+            for i in 0..self.parameters.len() {
+                bs.set_u16::<16>(in_range_u16(self.parameters[i], 12768, 52768));
+            }
+            bs.set_bits(&self.unused);
         })
     }
 
     fn from_bytes(bytes: Box<[u8; Self::BYTE_SIZE]>) -> Result<Self, BytesError> where Self: Sized {
         BitStream::read_fixed(bytes, |bs| {
-            Ok(Self(bs.get_bits()))
+            let enable = bs.get_bool();
+            let effect_type = bs.get_bits();
+            let mut parameters = [0; 32];
+            for i in 0..parameters.len() {
+                parameters[i] = bs.get_u16::<16>();
+            }
+            Ok(Self {
+                enable,
+                effect_type,
+                parameters,
+                unused: bs.get_bits()
+            })
         })
     }
 
