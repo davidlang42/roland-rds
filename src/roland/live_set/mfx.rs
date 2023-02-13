d8e23f98d4c6fa266a02e67e1cdf7decad367595
David Lang <davidlang42@gmail.com>
2023-02-14 00:04:39 +1100

    Finish implementing mfx

--- src/roland/live_set/mfx.rs
+++ src/roland/live_set/mfx.rs
@@ -7,37 +7,67 @@ use crate::roland::in_range_u16;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Mfx {
     enable: bool,
-    effect_type: Bits<92>, //TODO decode effect type (needs new test RDS file)
+    #[serde(skip_serializing_if="Bits::is_zero", default="Bits::zero")]
+    unused1: Bits<8>,
+    effect_type: u8,
+    #[serde(skip_serializing_if="Bits::is_unit", default="Bits::unit")]
+    padding1: Bits<8>,
+    #[serde(skip_serializing_if="Bits::is_unit", default="Bits::unit")]
+    padding2: Bits<14>,
+    #[serde(skip_serializing_if="Bits::is_unit", default="Bits::unit")]
+    padding3: Bits<14>,
+    #[serde(skip_serializing_if="Bits::is_unit", default="Bits::unit")]
+    padding4: Bits<14>,
+    #[serde(skip_serializing_if="Bits::is_zero", default="Bits::zero")]
+    unused2: Bits<26>,
     parameters: [u16; 32], // 12768-52768 (-20000 - +20000)
     #[serde(skip_serializing_if="Bits::is_zero", default="Bits::zero")]
-    unused: Bits<3>
+    unused3: Bits<3>
 }
 
 impl Bytes<76> for Mfx {
     fn to_bytes(&self) -> Box<[u8; Self::BYTE_SIZE]> {
         BitStream::write_fixed(|bs| {
             bs.set_bool(self.enable);
-            bs.set_bits(&self.effect_type);
+            bs.set_bits(&self.unused1);
+            bs.set_u8::<8>(self.effect_type);
+            bs.set_bits(&self.padding1);
+            bs.set_bits(&self.padding2);
+            bs.set_bits(&self.padding3);
+            bs.set_bits(&self.padding4);
+            bs.set_bits(&self.unused2);
             for i in 0..self.parameters.len() {
                 bs.set_u16::<16>(in_range_u16(self.parameters[i], 12768, 52768));
             }
-            bs.set_bits(&self.unused);
+            bs.set_bits(&self.unused3);
         })
     }
 
     fn from_bytes(bytes: Box<[u8; Self::BYTE_SIZE]>) -> Result<Self, BytesError> where Self: Sized {
         BitStream::read_fixed(bytes, |bs| {
             let enable = bs.get_bool();
-            let effect_type = bs.get_bits();
+            let unused1 = bs.get_bits();
+            let effect_type = bs.get_u8::<8>();
+            let padding1 = bs.get_bits();
+            let padding2 = bs.get_bits();
+            let padding3 = bs.get_bits();
+            let padding4 = bs.get_bits();
+            let unused2 = bs.get_bits();
             let mut parameters = [0; 32];
             for i in 0..parameters.len() {
                 parameters[i] = bs.get_u16::<16>();
             }
             Ok(Self {
                 enable,
+                unused1,
                 effect_type,
+                padding1,
+                padding2,
+                padding3,
+                padding4,
+                unused2,
                 parameters,
-                unused: bs.get_bits()
+                unused3: bs.get_bits()
             })
         })
     }
