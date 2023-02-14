366e21c7c8da600989fc9f087d04beef549fdb8c
David Lang <davidlang42@gmail.com>
2023-02-14 23:32:54 +1100

    Implement OutputPort

--- src/roland/live_set/song_rhythm.rs
+++ src/roland/live_set/song_rhythm.rs
@@ -2,6 +2,7 @@ use std::fmt::Debug;
 
 use crate::bytes::{Bytes, BytesError, Bits, BitStream};
 use crate::json::{Json, StructuredJson, StructuredJsonError};
+use crate::roland::types::OutputPort;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SongRhythm {
@@ -9,14 +10,14 @@ pub struct SongRhythm {
     unused1: Bits<1>,
     audio_level: u8, // max 127
     song_level: u8, // max 127
-    song_output_port: u8, // max 5 (ALL, INT, OUT1, OUT2, OUT3, USB)
+    song_output_port: OutputPort,
     #[serde(skip_serializing_if="Bits::is_zero", default="Bits::zero")]
     unused2: Bits<1>,
     rhythm_set: u8, // max 13
     rhythm_level: u8, // max 127
     rhythm_pattern: u8, // max 200
     rhythm_midi_out_channel: u8, // max 16 (OFF, 1-16)
-    rhythm_output_port: u8, // max 5 (ALL, INT, OUT1, OUT2, OUT3, USB)
+    rhythm_output_port: OutputPort,
     #[serde(skip_serializing_if="Bits::is_zero", default="Bits::zero")]
     unused3: Bits<2>
 }
@@ -27,13 +28,13 @@ impl Bytes<6> for SongRhythm {
             bits.set_bits(&self.unused1);
             bits.set_u8::<7>(self.audio_level, 0, 127)?;
             bits.set_u8::<7>(self.song_level, 0, 127)?;
-            bits.set_u8::<3>(self.song_output_port, 0, 5)?;
+            bits.set_u8::<3>(self.song_output_port.into(), 0, 5)?;
             bits.set_bits(&self.unused2);
             bits.set_u8::<4>(self.rhythm_set, 0, 13)?;
             bits.set_u8::<7>(self.rhythm_level, 0, 127)?;
             bits.set_u8::<8>(self.rhythm_pattern, 0, 200)?;
             bits.set_u8::<5>(self.rhythm_midi_out_channel, 0, 16)?;
-            bits.set_u8::<3>(self.rhythm_output_port, 0, 5)?;
+            bits.set_u8::<3>(self.rhythm_output_port.into(), 0, 5)?;
             bits.set_bits(&self.unused3);
             Ok(())
         })
@@ -45,13 +46,13 @@ impl Bytes<6> for SongRhythm {
                 unused1: data.get_bits(),
                 audio_level: data.get_u8::<7>(0, 127)?,
                 song_level: data.get_u8::<7>(0, 127)?,
-                song_output_port: data.get_u8::<3>(0, 5)?,
+                song_output_port: data.get_u8::<3>(0, 5)?.into(),
                 unused2: data.get_bits(),
                 rhythm_set: data.get_u8::<4>(0, 13)?,
                 rhythm_level: data.get_u8::<7>(0, 127)?,
                 rhythm_pattern: data.get_u8::<8>(0, 200)?,
                 rhythm_midi_out_channel: data.get_u8::<5>(0, 16)?,
-                rhythm_output_port: data.get_u8::<3>(0, 5)?,
+                rhythm_output_port: data.get_u8::<3>(0, 5)?.into(),
                 unused3: data.get_bits()
             })
         })
--- src/roland/types.rs
+++ src/roland/types.rs
@@ -1,3 +1,6 @@
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
 #[derive(Serialize, Deserialize, Debug, Copy, Clone)]
 pub struct Parameter(i16); // 12768-52768 (-20000 - +20000)
 
@@ -28,5 +31,33 @@ impl Into<u16> for Parameter {
 impl Default for Parameter {
     fn default() -> Self {
         Self::from(Self::ZERO)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, EnumIter)]
+pub enum OutputPort { // 0-5
+    ALL,
+    INT,
+    OUT1,
+    OUT2,
+    OUT3,
+    USB
+}
+
+impl From<u8> for OutputPort {
+    fn from(value: u8) -> Self {
+        Self::iter().nth(value as usize).unwrap()
+    }
+}
+
+impl Into<u8> for OutputPort {
+    fn into(self) -> u8 {
+        Self::iter().position(|s| s == self).unwrap() as u8
+    }
+}
+
+impl Default for OutputPort {
+    fn default() -> Self {
+        Self::from(0)
     }
 }
