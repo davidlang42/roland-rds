e219404d7e211a5bf417a8e63d30d410edc22bb5
David Lang <davidlang42@gmail.com>
2023-07-26 23:51:48 +1000

    Finish validation helpers

--- src/json/validation.rs
+++ src/json/validation.rs
@@ -3,31 +3,51 @@ use std::{borrow::Cow, collections::HashMap};
 use serde::Serialize;
 use strum::IntoEnumIterator;
 use validator::{ValidationError, Validate, ValidationErrors};
+use std::hash::Hash;
+use std::cmp::Eq;
 
 pub fn valid_chars<const N: usize>(chars: &[char; N]) -> Result<(), ValidationError> {
     const MIN: u8 = 32;
     const MAX: u8 = 127;
+    let mut invalid = Vec::new();
     for (i, c) in chars.iter().enumerate() {
         let ascii = *c as u8;
         if ascii < MIN || ascii > MAX {
-            let mut e = ValidationError::new("Chracter out of range");
-            e.add_param(Cow::from("Min"), &MIN);
-            e.add_param(Cow::from("Max"), &MAX);
-            e.add_param(Cow::from("CharIndex"), &i);
-            return Err(e);
+            invalid.push(i);
         }
     }
-    Ok(())
+    if invalid.len() == 0 {
+        Ok(())
+    } else {
+        let mut e = ValidationError::new("Characters out of range");
+        e.add_param(Cow::from("Min"), &MIN);
+        e.add_param(Cow::from("Max"), &MAX);
+        e.add_param(Cow::from("CharIndicies"), &invalid);
+        Err(e)
+    }
 }
 
 pub fn validate_boxed_array<T: Validate, const N: usize>(boxed_array: &Box<[T; N]>) -> Vec<Result<(), ValidationErrors>> {
     boxed_array.as_ref().iter().map(|t| t.validate()).collect()
 }
 
-pub fn valid_boxed_elements<T: Validate, const N: usize>(_boxed_array: &Box<[T; N]>) -> Result<(), ValidationError> {
-    //boxed_array.as_ref().iter().map(|t| t.validate()).collect()
-    //TODO implement
-    todo!()
+pub fn valid_boxed_elements<T: Validate, const N: usize>(boxed_array: &Box<[T; N]>) -> Result<(), ValidationError> {
+    let mut errors = Vec::new();
+    for (i, t) in boxed_array.iter().enumerate() {
+        if let Err(error) = t.validate() {
+            errors.push((i, error));
+        }
+    }
+    if errors.len() == 0 {
+        Ok(())
+    } else {
+        let mut e = ValidationError::new("Boxed array element(s) failed validation");
+        let indicies: Vec<usize> = errors.iter().map(|(x, _)| *x).collect();
+        e.add_param(Cow::from("ArrayIndicies"), &indicies);
+        let error_objects: Vec<ValidationErrors> = errors.into_iter().map(|(_, x)| x).collect();
+        e.add_param(Cow::from("Errors"), &error_objects);
+        Err(e)
+    }
 }
 
 pub fn validate_control_change(cc: &u8) -> Result<(), ValidationErrors> {
@@ -38,9 +58,20 @@ pub fn validate_control_change(cc: &u8) -> Result<(), ValidationErrors> {
     }
 }
 
-pub fn contains_all_keys<K: IntoEnumIterator, V>(_map: &HashMap<K, V>) -> Result<(), ValidationError> {
-    //TODO implement
-    todo!()
+pub fn contains_all_keys<K: IntoEnumIterator + Hash + Eq + Serialize, V>(map: &HashMap<K, V>) -> Result<(), ValidationError> {
+    let mut missing = Vec::new();
+    for key in K::iter() {
+        if !map.contains_key(&key) {
+            missing.push(key);
+        }
+    }
+    if missing.len() == 0 {
+        Ok(())
+    } else {
+        let mut e = ValidationError::new("HashMap missing required keys");
+        e.add_param(Cow::from("MissingKeys"), &missing);
+        Err(e)
+    }
 }
 
 pub fn out_of_range_err<T: Serialize>(field: &'static str, min: &T, max: &T) -> ValidationErrors {
