05590f4164785fcaeec6c4d937fdb138b0b4ddad
David Lang <davidlang42@gmail.com>
2023-08-08 16:07:58 +1000

    LogFrequency WIP

--- src/roland/types/effects.rs
+++ src/roland/types/effects.rs
@@ -114,14 +114,15 @@ impl Validate for ChorusType {
 #[derive(Serialize, Deserialize, Debug, JsonSchema, Validate)]
 pub struct ChorusParameters {//TODO add validation
     filter_type: FilterType,
-    cutoff_frequency: Frequency,
-    pre_delay: Milliseconds,
+    cutoff_frequency: LogarithmicFrequency<200, 8000, 800>,
+    pre_delay: Milliseconds, // default 2.0
     rate_mode: TimingMode,
-    rate_hz: f64,
-    rate_note: NoteLength,
-    depth: u8,
-    phase: u8,
-    feedback: u8,
+    #[validate(range(min = 0.05, max = 10.0))] // by 0.05
+    rate_hz: f64,//default 1
+    rate_note: NoteLength,//default whole note
+    depth: u8,//0-127, default 40
+    phase: u8,//0-180, default 180
+    feedback: u8,//0-127, default 8
     #[serde(deserialize_with = "serialize_default_terminated_array::deserialize")]
     #[serde(serialize_with = "serialize_default_terminated_array::serialize")]
     #[schemars(with = "serialize_default_terminated_array::DefaultTerminatedArraySchema::<Parameter, 11>")]
@@ -155,19 +156,43 @@ impl Parameters<20> for ChorusParameters {
 
 //TODO move things below this into enums/numeric/etc
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
-enum Frequency {
-    //TODO
+struct LogarithmicFrequency<const MIN: u16, const MAX: u16, const DEFAULT: u16>(u16);
+
+impl<const L: u16, const H: u16, const D: u16> LogarithmicFrequency<L, H, D> {
+    const BASE_VALUES: [u16; 10] = [200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600];
+
+    fn values() -> Vec<u16> {
+        let mut factor = 1;
+        let mut v = Vec::new();
+        loop {
+            for base_value in Self::BASE_VALUES {
+                let current = base_value * factor;
+                if current >= L {
+                    if current <= H {
+                        v.push(current);
+                    } else {
+                        return v;
+                    }
+                }
+            }
+            factor *= 10;
+        }
+    }
 }
 
-impl From<Parameter> for Frequency {
+impl<const L: u16, const H: u16, const D: u16> From<Parameter> for LogarithmicFrequency<L, H, D> {
     fn from(value: Parameter) -> Self {
-        todo!() //TODO
+        let values = Self::values();
+        if value.0 < 0 || value.0 >= values.len() {
+            panic!("Parameter out of range: {} (expected 0-{})", value.0, values.len()-1)
+        }
+        values.iter().nth(value.0).unwrap();
     }
 }
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
-enum Milliseconds {
-    //TODO
+enum LogMilliseconds {
+    //TODO 0-5 by 0.1, 5-10 by 0.5, 10-50 by 1, 50-100 by 2
 }
 
 impl From<Parameter> for Milliseconds {
@@ -178,7 +203,8 @@ impl From<Parameter> for Milliseconds {
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 enum TimingMode {
-    //TODO
+    Hertz,
+    Note
 }
 
 impl From<Parameter> for TimingMode {
@@ -189,7 +215,28 @@ impl From<Parameter> for TimingMode {
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 enum NoteLength {
-    //TODO
+    SixtyFourthNoteTriplet,
+    SixtyFourthNote,
+    ThirtySecondNoteTriplet,
+    ThirtySecondNote,
+    SixteenthNoteTriplet,
+    DottedThirtySecondNote,
+    SixteenthNote,
+    EighthNoteTriplet,
+    DottedSixteenthNote,
+    EighthNote,
+    QuarterNoteTriplet,
+    DottedEighthNote,
+    QuarterNote,
+    HalfNoteTriplet,
+    DottedQuarterNote,
+    HalfNote,
+    WholeNoteTriplet,
+    DottedHalfNote,
+    WholeNote,
+    DoubleNoteTriplet,
+    DottedWholeNote,
+    DoubleNote
 }
 
 impl From<Parameter> for NoteLength {
@@ -197,3 +244,47 @@ impl From<Parameter> for NoteLength {
         todo!() //TODO
     }
 }
+
+//TODO future
+
+// struct DelayParameters {
+//     delay_left_mode: DelayMode,//default note
+//     delay_left_ms: u16, //1-1000 by 1, default 200
+//     delay_left_note: NoteLength, // default eighth note triplet
+//     delay_right_mode: DelayMode,//default note
+//     delay_right_ms: u16, //1-1000 by 1, default 400
+//     delay_right_note: NoteLength,//default quarternote triplet
+//     delay_centre_mode: DelayMode,//default note
+//     delay_centre_ms: u16, //1-1000 by 1, default 600
+//     delay_centre_note: NoteLength, // default quarternote
+//     centre_feedback: u8, //-98% to 98% by 2, default +20
+//     hf_damp: u16, //200-800 logarthimic LogarithmicFrequency + bypass default bypass
+//     left_level: u8, //0-127, default 127
+//     right_level: u8, //0-127, default 127
+//     centre_level: u8, //0-127, default 127
+//     #[serde(deserialize_with = "serialize_default_terminated_array::deserialize")]
+//     #[serde(serialize_with = "serialize_default_terminated_array::serialize")]
+//     #[schemars(with = "serialize_default_terminated_array::DefaultTerminatedArraySchema::<Parameter, 6>")]
+//     #[validate(custom = "valid_boxed_elements")]
+//     unused_parameters: Box<[Parameter; 6]>
+// }
+
+// enum DelayMode { //TODO better name
+//     Milliseconds,
+//     Note
+// }
+
+// struct Gm2ChorusParameters {
+//     pre_lpf: u8,//0-7
+//     level: u8: //0-127, default 64
+//     feedback: u8, //0-127, default 8
+//     delay: u8, //0-127, default 80
+//     rate: u8, //0-127, default 3
+//     depth: u8, //0-127, default 19
+//     send_to_reverb: u8, //0-127
+//     #[serde(deserialize_with = "serialize_default_terminated_array::deserialize")]
+//     #[serde(serialize_with = "serialize_default_terminated_array::serialize")]
+//     #[schemars(with = "serialize_default_terminated_array::DefaultTerminatedArraySchema::<Parameter, 13>")]
+//     #[validate(custom = "valid_boxed_elements")]
+//     unused_parameters: Box<[Parameter; 13]>
+// }
